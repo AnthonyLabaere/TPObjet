5bc55ed0ac5b3e8c11dfe9e196eb55a81fa587d7
Anthony <anthony.labaere@eleves.ec-nantes.fr>
2013-09-27 16:57:41 +0200

    strzerh

---
+++ src/PGM.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public class PGM {
+
+	private String name;
+	private int hauteur;
+	private int largeur;
+
+	private List<Integer> pixel;
+
+	public PGM(String fileName) {
+		try {
+			InputStream ips = new FileInputStream(fileName);
+			InputStreamReader ipsr = new InputStreamReader(ips);
+			BufferedReader br = new BufferedReader(ipsr);
+			String ligne;
+			int numeroLine = 0;
+			
+			while ((ligne = br.readLine()) != null) {
+				numeroLine++;
+			}
+			
+			br.close();
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+	}
+
+	public void lire() {
+
+	}
+
+}
